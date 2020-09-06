6036053f9c1eb323395e38490370b4b7e4a39518
William Schwindt <williammschwindt@gmail.com>
2020-09-06 12:48:10 -0700

    cpu loads files dynamically

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -21,21 +21,26 @@ class CPU:
 
         address = 0
 
-        # For now, we've just hardcoded a program:
+        if len(sys.argv) != 2:
+            print('usage: cpu.py filename')
+            sys.exit(1)
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        try:
+            with open(sys.argv[1]) as f:
+                for line in f:
+                    comment_split = line.split('#')
+                    code_snip = comment_split[0].strip()
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+                    if code_snip == '':
+                        continue
+                    
+                    binary = int(code_snip, 2)
+                    self.ram[address] = binary
+                    address += 1
+
+        except FileNotFoundError:
+            print(f'{sys.argv[1]} could not be found')
+            sys.exit(2)
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""

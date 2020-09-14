8206f4a681010d1935db029a188d85212f3ee6d7
William Schwindt <williammschwindt@gmail.com>
2020-09-13 18:57:32 -0700

    interrupts attempted

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,6 +1,7 @@
 """CPU functionality."""
 
 import sys
+from datetime import datetime
 
 class CPU:
     """Main CPU class."""
@@ -12,7 +13,13 @@ class CPU:
         self.pc = 0
         self.sp = 7
         self.reg[self.sp] = 0xf4
-        self.fl = 0b00000000
+        self.IS = 6
+        self.reg[self.IS] = 0b00000000
+        self.IM = 5
+        self.reg[self.IM] = 0b00000000
+        self.fl = 4
+        self.reg[self.fl] = 0b00000000
+        self.time = int(datetime.now().strftime("%Y-%m-%d %H:%M:%S")[-2:])
         self.branch_table = {}
         self.branch_table[0b10000010] = self.LDI
         self.branch_table[0b01000111] = self.PRN
@@ -27,6 +34,9 @@ class CPU:
         self.branch_table[0b01010100] = self.JMP
         self.branch_table[0b01010101] = self.JEQ
         self.branch_table[0b01010110] = self.JNE
+        self.branch_table[0b10000100] = self.ST
+        self.branch_table[0b01001000] = self.PRA
+        self.branch_table[0b00010011] = self.IRET
         
     def ram_read(self, address):
         return self.ram[address]
@@ -62,7 +72,7 @@ class CPU:
 
     def PUSH(self):
         operand_a = self.ram[self.pc + 1]
-        self.sp -= 1
+        self.reg[self.sp] -= 1
         value = self.reg[operand_a]
         self.ram[self.sp] = value
         self.pc += 2
@@ -71,7 +81,7 @@ class CPU:
         operand_a = self.ram[self.pc + 1]
         value = self.ram[self.sp]
         self.reg[operand_a] = value
-        self.sp += 1
+        self.reg[self.sp] += 1
         self.pc += 2
 
     def CALL(self):
@@ -98,7 +108,7 @@ class CPU:
         self.pc = address
 
     def JEQ(self):
-        mask = bin(0b00000001 & self.fl)
+        mask = bin(0b00000001 & self.reg[self.fl])
         if mask == '0b1':
             reg_a = self.ram[self.pc + 1]
             address = self.reg[reg_a]
@@ -107,13 +117,55 @@ class CPU:
             self.pc += 2
 
     def JNE(self):
-        mask = bin(0b00000001 & self.fl)
+        mask = bin(0b00000001 & self.reg[self.fl])
         if mask == '0b0':
             reg_a = self.ram[self.pc + 1]
             address = self.reg[reg_a]
             self.pc = address
         else:
             self.pc += 2
+
+    def ST(self):
+        operand_a = self.ram[self.pc + 1]
+        operand_b = self.ram[self.pc + 2]
+        address = self.reg[operand_a]
+        value = self.reg[operand_b]
+        self.ram[address] = value
+        self.pc += 3
+
+    def PRA(self):
+        operand_a = self.ram[self.pc + 1]
+        value = self.reg[operand_a]
+        if value == 65:
+            print('A')
+        self.pc += 2
+
+    def IRET(self):
+        #pop all registers off stack
+        self.ram[self.reg[self.sp]] = 0
+        self.reg[self.sp] += 1
+        self.ram[self.reg[self.sp]] = 0
+        self.reg[self.sp] += 1
+        self.ram[self.reg[self.sp]] = 0
+        self.reg[self.sp] += 1
+        self.ram[self.reg[self.sp]] = 0
+        self.reg[self.sp] += 1
+        self.ram[self.reg[self.sp]] = 0
+        self.reg[self.sp] += 1
+        self.ram[self.reg[self.sp]] = 0
+        self.reg[self.sp] += 1
+        self.ram[self.reg[self.sp]] = 0
+        self.reg[self.sp] += 1
+        #fl popped off
+        self.ram[self.reg[self.sp]] = 0
+        self.reg[self.sp] += 1
+        #return address
+        address = self.ram[self.reg[self.sp]]
+        self.ram[self.reg[self.sp]] = 0
+        self.pc = address
+        #interrupts
+        self.time = int(datetime.now().strftime("%Y-%m-%d %H:%M:%S")[-2:])
+
 
     def load(self):
         """Load a program into memory."""
@@ -152,11 +204,11 @@ class CPU:
 
         elif op == "COMPARE":
             if self.reg[reg_a] < self.reg[reg_b]:
-                self.fl = 0b00000100
+                self.reg[self.fl] = 0b00000100
             elif self.reg[reg_a] > self.reg[reg_b]:
-                self.fl = 0b00000010
+                self.reg[self.fl] = 0b00000010
             elif self.reg[reg_a] == self.reg[reg_b]:
-                self.fl = 0b00000001
+                self.reg[self.fl] = 0b00000001
 
         else:
             raise Exception("Unsupported ALU operation")
@@ -186,13 +238,45 @@ class CPU:
         self.running = True
     
         while self.running:
-            IR = self.ram[self.pc]
-            # print(bin(IR))
- 
-            if IR in self.branch_table:
-                # print(self.branch_table[self.ram[self.pc]])
-                self.branch_table[IR]()
+            if int(datetime.now().strftime("%Y-%m-%d %H:%M:%S")[-2:]) == self.time + 1:
+                self.reg[self.IS] = 0b00000001
+
+            elif bin(self.reg[self.IS]) == '0b1' :
+                masked_interrupts = self.reg[self.IM] & self.reg[self.IS]
+
+                for i in range(8):
+                    interrupt_happened = ((masked_interrupts >> i) & 1) == 1
+                    if interrupt_happened:
+                        self.reg[self.IS] = 0b00000000
+                        self.ram[self.reg[self.sp]] = self.pc
+                        self.reg[self.sp] -= 1
+                        self.ram[self.reg[self.sp]] = self.reg[self.fl]
+                        self.reg[self.sp] -= 1
+                        self.ram[self.reg[self.sp]] = self.reg[0]
+                        self.reg[self.sp] -= 1
+                        self.ram[self.reg[self.sp]] = self.reg[1]
+                        self.reg[self.sp] -= 1
+                        self.ram[self.reg[self.sp]] = self.reg[2]
+                        self.reg[self.sp] -= 1
+                        self.ram[self.reg[self.sp]] = self.reg[3]
+                        self.reg[self.sp] -= 1
+                        self.ram[self.reg[self.sp]] = self.reg[4]
+                        self.reg[self.sp] -= 1
+                        self.ram[self.reg[self.sp]] = self.reg[5]
+                        self.reg[self.sp] -= 1
+                        self.ram[self.reg[self.sp]] = self.reg[6]
+                        self.pc = self.ram[0xf8]
+
+                continue
 
             else:
-                print(f'unknown command {IR}')
-                self.running = False
+                IR = self.ram[self.pc]
+                print(bin(IR))
+    
+                if IR in self.branch_table:
+                    # print(self.branch_table[self.ram[self.pc]])
+                    self.branch_table[IR]()
+
+                else:
+                    print(f'unknown command {IR}')
+                    self.running = False
